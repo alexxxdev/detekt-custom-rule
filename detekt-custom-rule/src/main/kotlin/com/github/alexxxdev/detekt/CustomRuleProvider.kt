37ced0533fe26ca2636724c54f67bb4f6020e825
a.derendyaev <alexxxdev@yandex.ru>
2020-08-20 12:55:59 +0700

    TodoCommentRule

--- detekt-custom-rule/src/main/kotlin/com/github/alexxxdev/detekt/CustomRuleProvider.kt
+++ detekt-custom-rule/src/main/kotlin/com/github/alexxxdev/detekt/CustomRuleProvider.kt
@@ -1,6 +1,7 @@
 package com.github.alexxxdev.detekt
 
 import com.github.alexxxdev.detekt.rules.RxJavaSubscriptionRule
+import com.github.alexxxdev.detekt.rules.TodoCommentRule
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
@@ -8,5 +9,10 @@ import io.gitlab.arturbosch.detekt.api.RuleSetProvider
 class CustomRuleProvider : RuleSetProvider {
     override val ruleSetId: String = "CodeStyle"
 
-    override fun instance(config: Config) = RuleSet(ruleSetId, listOf(RxJavaSubscriptionRule(config)))
+    override fun instance(config: Config) = RuleSet(ruleSetId,
+        listOf(
+            RxJavaSubscriptionRule(config),
+            TodoCommentRule(config)
+        )
+    )
 }
